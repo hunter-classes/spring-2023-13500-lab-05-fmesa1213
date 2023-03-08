01eabd9924fdb170efe0f98be1a225bbae4d899d
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2023-03-07 21:22:56 -0500

    Create numbers.cpp

---
+++ numbers.cpp
@@ -0,0 +1,8 @@
+/*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: 
+Assignment: Lab5A
+Wrote a program that defines a function bool isDivisibleBy(int n, int d);
+If n is divisible by d, the function should return true, otherwise return false.
+*/
