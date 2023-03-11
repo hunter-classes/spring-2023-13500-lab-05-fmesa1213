fc6a8313a9764c4a01b1f48b9ef55379d760e561
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2023-03-10 22:56:55 -0500

    Update numbers.cpp

--- numbers.cpp
+++ numbers.cpp
@@ -6,3 +6,44 @@ Assignment: Lab5A
 Wrote a program that defines a function bool isDivisibleBy(int n, int d);
 If n is divisible by d, the function should return true, otherwise return false.
 */
+
+#include <iostream>
+using namespace std;
+
+bool isDivisibleBy(int n, int d){
+
+  if(d==0)return false;
+
+  if(n%d==0)
+{
+  return true;
+}
+else
+{
+  return false;
+}
+}
+
+int main(){
+
+int num;
+int div;
+
+cout << "Enter the value of dividend: " << endl;
+cin >> num;
+
+cout << "Enter the value of divisor: " << endl;
+cin >> div;
+
+bool ans = isDivisibleBy(num,div);
+
+if(ans)
+{
+  cout<<"YES";
+}
+else
+{
+  cout<<"NO";
+}
+return 0;
+}
